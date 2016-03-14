e02ecbb697ab6799ccc0809331c40f54f877fc96
Steven Magadan <steven.magadan@hotmail.com>
2016-03-14 14:27:21 -0700

    Added auth

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,18 @@
 var
   express = require('express'),
   userRouter = express.Router(),
+  passport = require('passport'),
   userCtrl = require('../controllers/users.js')
 
 
-userRouter.get('/:id', userCtrl.profile)
+  userRouter.get('/:id', userCtrl.profile)
+
+  userRouter.get('/auth/facebook', userCtrl.facebookEmail)
+
+  userRouter.get('/auth/facebook/callback', userCtrl.faceBookLogin)
 
 
-module.exports = userRouter
+
+
+
+  module.exports = userRouter
