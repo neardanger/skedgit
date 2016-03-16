17aaa582bf12194a0969487591ab053bef6babf5
Arman Sani <armanisani@gmail.com>
2016-03-16 15:05:52 -0700

    working on map

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -36,12 +36,9 @@ v.addEventListener("timeupdate", function() {
 }, false)
 // users inputed location
 
-<<<<<<< HEAD
-=======
 var whereTo
 
 $('img').addClass('materialboxed')
->>>>>>> f61373a0e8654bb662748724183b5432c8348536
 // switch between stages
 $('#submit-location').click(function(evt){
   slideBetween('#stage1','#stage2')
@@ -93,17 +90,6 @@ function addSlider(i){
   })
 }
 
-<<<<<<< HEAD
-$(document).ready(function(){
-   // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
-   $('.modal-trigger').leanModal();
-   function addScheduleItem(){
-
-   }
-   $('#add-schedule-item').click(addScheduleItem)
- });
-=======
->>>>>>> f61373a0e8654bb662748724183b5432c8348536
 
 var query = []
 
@@ -249,7 +235,58 @@ $('#stage2-submit').click(function(){
     })
 
 
+    function initMap() {
+      var directionsService = new google.maps.DirectionsService;
+      var directionsDisplay = new google.maps.DirectionsRenderer;
+      var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 6,
+        center: {lat: 41.85, lng: -87.65}
+      });
+      directionsDisplay.setMap(map);
 
+      document.getElementById('submit').addEventListener('click', function() {
+        calculateAndDisplayRoute(directionsService, directionsDisplay);
+      });
+    }
+
+    function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+      var waypts = [];
+      var checkboxArray = document.getElementById('waypoints');
+      for (var i = 0; i < checkboxArray.length; i++) {
+        if (checkboxArray.options[i].selected) {
+          waypts.push({
+            location: checkboxArray[i].value,
+            stopover: true
+          });
+        }
+      }
+
+      directionsService.route({
+        origin: document.getElementById('start').value,
+        destination: document.getElementById('end').value,
+        waypoints: waypts,
+        optimizeWaypoints: true,
+        travelMode: google.maps.TravelMode.DRIVING
+      }, function(response, status) {
+        if (status === google.maps.DirectionsStatus.OK) {
+          directionsDisplay.setDirections(response);
+          var route = response.routes[0];
+          var summaryPanel = document.getElementById('directions-panel');
+          summaryPanel.innerHTML = '';
+          // For each route, display summary information.
+          for (var i = 0; i < route.legs.length; i++) {
+            var routeSegment = i + 1;
+            summaryPanel.innerHTML += '<b>Route Segment: ' + routeSegment +
+                '</b><br>';
+            summaryPanel.innerHTML += route.legs[i].start_address + ' to ';
+            summaryPanel.innerHTML += route.legs[i].end_address + '<br>';
+            summaryPanel.innerHTML += route.legs[i].distance.text + '<br><br>';
+          }
+        } else {
+          window.alert('Directions request failed due to ' + status);
+        }
+      });
+    }
   // stage 4  //////////////////////////////////////////////////////////
   var prop = $('#searchProp')
   var tr = $('#trList')
