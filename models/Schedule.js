92946066a425ebe4b68f13d5a45783e078633e69
Steven Magadan <steven.magadan@hotmail.com>
2016-03-16 15:42:51 -0700

    changed relationships

--- models/Schedule.js
+++ models/Schedule.js
@@ -15,7 +15,7 @@ var businessSchema = new mongoose.Schema({
 })
 
 var scheduleSchema = new mongoose.Schema({
-  user:[{type: mongoose.Schema.Types.ObjectId, ref: 'User'}]
+  user:[{type: mongoose.Schema.Types.ObjectId, ref: 'User'}],
   name: {type: String, required: true},
   businesses: [businessSchema]
 
